a644c8d00b157ad4d60c9f5ac8aeecea1f6a0271
SkylinVale <skylinvale@gmail.com>
2026-09-06 12:10:12 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1920,7 +1920,7 @@ async def test_ocr(
 
         data = pytesseract.image_to_data(
             img,
-            config="--psm 6",
+            config="--psm 11",
             output_type=Output.DICT
         )
 
